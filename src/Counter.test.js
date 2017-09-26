1c8fc46c48e360ace3d0be54a56d7180cbac7827
carole.olivier <carole.olivier@gmail.com>
2017-09-26 11:10:41 +0100

    Add test for testing Counter component renders correctly

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import Counter from './Counter';
 
 describe('Counter component', () => {
-    test('renders correctly', () => {
+    test('renders without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(<Counter/>, div);
+    });
+
+    test('renders according to snapshot', () => {
+        const component = renderer.create(
+            <Counter></Counter>
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot("rendering");
     });
 });
