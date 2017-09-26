3a51aa504aa54c070616d080cc5efdad076b112d
carole.olivier <carole.olivier@gmail.com>
2017-09-26 11:51:17 +0100

    Add comment in tests

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer';
+import testRenderer from 'react-test-renderer';
 import Counter from './Counter';
 
 describe('Counter component', () => {
     test('renders without crashing', () => {
+        // test with a DOM and renders deeply (as opposed to a shallowly, i.e. one level deep)
         const div = document.createElement('div');
         ReactDOM.render(<Counter/>, div);
     });
 
     test('renders according to snapshot', () => {
-        const component = renderer.create(
-            <Counter></Counter>
+        // rendering doesn't depend on the DOM
+        const component = testRenderer.create(
+            <Counter/>
         );
         let tree = component.toJSON();
         expect(tree).toMatchSnapshot("rendering");
