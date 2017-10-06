9d42c650ff5be302a656e794ce7fc1b15678117e
carole.olivier <carole.olivier@gmail.com>
2017-10-06 13:04:53 +0100

    Add styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require("path");
+const path = require('path');
 
 // Transpiling and Bundling config
 module.exports = {
-    context: path.join(__dirname, "src"),
+    context: path.join(__dirname, 'src'),
     entry: [
-        "./main.js",
+        './main.js'
     ],
     output: {
-        path: path.join(__dirname, "www"),
-        filename: "bundle.js"
+        path: path.join(__dirname, 'www'),
+        filename: 'bundle.js'
     },
     module: {
         rules: [
@@ -16,17 +16,35 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: '[path][name]__[local]--[hash:base64:5]'
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
             }
         ]
     },
-    resolve:{
+    resolve: {
         modules: [
-            path.join(__dirname, "node_modules"),
+            path.join(__dirname, 'node_modules')
         ]
     }
-}
+};
 
 // For local development
 module.exports.devServer = {
