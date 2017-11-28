cc26219ff31f66f45c3ed1d0d1af371dd2548a3c
carole.olivier <carole.olivier@gmail.com>
2017-11-28 12:24:40 +0000

    Improve typescript react component

--- src/Greeter.tsx
+++ src/Greeter.tsx
@@ -5,8 +5,31 @@ interface GreeterProps {
     name: string;
 }
 
-export class Greeter extends React.Component<GreeterProps, {}> {
-    render() {
-        return <h1>Hello {this.props.name}</h1>;
+interface GreeterState {
+    counter: number;
+}
+
+export class Greeter extends React.Component<GreeterProps, GreeterState> {
+
+    constructor(props: GreeterProps) {
+        super(props);
+        const initialState: Readonly<GreeterState> = { counter: 0};
+        this.state = initialState;
+        this.click = this.click.bind(this);
+    }
+
+    render(): JSX.Element {
+        return (
+            <div>
+                <h1>Hello {this.props.name}</h1>
+                <h2>{this.state.counter}</h2>
+                <button onClick={this.click}>click me</button>
+            </div>
+        );
+    }
+
+    click() {
+        const newState: Pick<GreeterState, "counter"> = { counter: this.state.counter+1};
+        this.setState(newState);
     }
 }
